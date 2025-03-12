4397fc53c17258cb7335079299af748e7e693e5a
Lorenous <osamaabaelziz@gmail.com>
2025-03-12 11:27:57 +0200

    create note item

---
+++ lib/widgets/notes_view_widgets/note_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  NoteItem({super.key});
+  DateTime currentDate = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color(0xffFFCC80),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 24, right: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding: const EdgeInsets.all(0),
+              title: const Text(
+                'Flutter tips',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              subtitle: const Padding(
+                padding: EdgeInsets.only(top: 16, right: 16),
+                child: Text(
+                  'Build Your Career With Tharwat Samy',
+                  style: TextStyle(
+                    color: Colors.black45,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Padding(
+              padding: EdgeInsets.only(right: 16),
+              child: Text(
+                'May 21,2022',
+                style: TextStyle(color: Colors.black45),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/notes_view_widgets/notes_view_body.dart
+++ lib/widgets/notes_view_widgets/notes_view_body.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/notes_view_widgets/note_item.dart';
 import 'package:notes_app/widgets/notes_view_widgets/notes_view_app_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           children: [
-            NotesViewAppBar(),
+            const NotesViewAppBar(),
+            NoteItem(),
           ],
         ),
       ),
